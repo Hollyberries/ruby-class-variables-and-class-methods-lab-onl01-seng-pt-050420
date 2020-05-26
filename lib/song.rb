1b85d9c3915f038bf55d4be0473b0b56874543ce
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-26 19:24:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,8 +31,8 @@ attr_accessor :name, :artist, :genre
   def self.genre_count
      genre_list = {}
      @@genres.each do |genre|
-     if genre_list.include?(:genre)
-       
+     if genre_list.include?(:genre + 1)
+      
     end
     end
   
