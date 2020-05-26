1e041c83a56254c50098618d1c34ce0b64f52763
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-26 17:17:47 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+
+@@count = 0
+
+attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+  @@count += 1
+  end
+
+  def self.count
+    
+  end
+  
+end
+
+
+
+
+#Album.new
+#Album.new
+#Album.new
+ 
+#Album.count
